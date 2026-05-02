1e851d34c5ad57e09e1b5c69cfffd2e0828421c7
FlaXDeR <flavioderose04@gmail.com>
2026-05-02 16:41:37 +0200

    Aggiunta HomePage e inizio

---
+++ src/frontend/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <main>
+            <h1>Mermaid Generator</h1>
+            <p>Genera diagrammi Mermaid partendo da testo o codice.</p>
+        </main>
+    );
+}
